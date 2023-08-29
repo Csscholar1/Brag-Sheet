2aa8511bc468a1513ee1f3128ec058e96cc2b929
Csscholar1 <137843020+Csscholar1@users.noreply.github.com>
2023-08-29 17:12:46 -0400

    Create home.js

---
+++ home.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('#exam').onclick = function() {
+    window.location = 'https://csscholar1-fantastic-fishstick-qwww5jppqw5c674j-8080.app.github.dev/exam.html';
+    }
+
+    document.querySelector('#essay').onclick = function() {
+        window.location = 'https://csscholar1-fantastic-fishstick-qwww5jppqw5c674j-8080.app.github.dev/essay.html';
+    }
+
+    document.querySelector('#extra').onclick = function() {
+        window.location = 'https://csscholar1-fantastic-fishstick-qwww5jppqw5c674j-8080.app.github.dev/extra-curricular.html';
+    }
+
+    document.querySelector('#list').onclick = function() {
+        window.location = 'https://csscholar1-fantastic-fishstick-qwww5jppqw5c674j-8080.app.github.dev/college-list.html';
+    }
+
+    document.querySelector('#home').onclick = function() {
+        window.location = 'https://csscholar1-fantastic-fishstick-qwww5jppqw5c674j-8080.app.github.dev/index.html';
+    }
+
+    document.querySelector('#common').onclick = function() {
+        window.location = 'https://docs.google.com/document/d/15r4xJnc4wyaess3lE6SWgfjOOc3HDzapqWGbtmqSuRo/edit?usp=sharing';
+    }
+});
